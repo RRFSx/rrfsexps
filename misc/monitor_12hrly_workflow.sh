b29917fdbd6a6513025168bc78fcfac5ac7ea2f2
guoqing.ge <Guoqing.Ge@noaa.gov>
2026-02-16 21:12:07 -0700

    add the capability to check hung fcst jobs in monitor_12hrly_workflow.sh

--- misc/monitor_12hrly_workflow.sh
+++ misc/monitor_12hrly_workflow.sh
@@ -2,6 +2,7 @@
 source /etc/profile
 module load rocoto
 cd /scratch4/BMC/zrtrr/gge/ARPS/PEAR/exp/rrfsdet
+source qrocoto/load_qrocoto.sh
 recipents="Guoqing.Ge@noaa.gov Guoqing.Ge@noaa.gov"
 subject="rt_PEAR alert"
 
@@ -14,10 +15,10 @@ cyc=$(printf "%02d" ${cyc})
 prevCyc=$(date -u -d "${PDY} ${cyc} UTC -24 hours" +%Y%m%d%H)
 cycles=${prevCyc}00:${PDY}${cyc}00
 
+## check whether we have dead jobs in the past 24 hours
 send_email=false
 msg=$(rocotostat -w rrfs.xml -d rrfs.db -c ${cycles})
 echo "${msg}" > .msg.new
-
 if [[ -s .msg.save  && -s .msg.new ]]; then
   if ! diff .msg.save .msg.new &>/dev/null && [[ ${msg} == *DEAD* ]]; then
     send_email=true
@@ -45,6 +46,30 @@ if [[ ${msg} != *RUNNING* ]]; then
 else
   rm -rf .stall
 fi
+
+## check whether any running fcst jobs hang without any further outputs
+rrun #&>/dev/null # update the rocoto db files first before rtasks
+running=$(rtasks fcst 48 | grep "RUNNING" | awk '{print $1","$3}')
+mapfile -t lines  <<< "${running}"
+for line in ${lines[@]}; do
+  cdate=${line:0:12}
+  jobid=${line:13}
+  cyc=${cdate:8:2}
+  dir=$(taskinfo ${cdate} fcst | tail -n 1)
+  log="${dir}/fcst_${cyc}/log.atmosphere.0000.out"
+  log_secs=$( stat -c %Y ${log} )
+  cur_secs=$( date +%s )
+  diff_secs=$(( cur_secs - log_secs ))
+  if (( diff_secs > 1200 )); then # hang for more than 20 minutes
+    # cancel the hung fcst task and reboot it
+    send_email=true
+    subject="${subject}: ${line:6:4}_hang"
+    scancel ${jobid} #&>/dev/null
+    sleep 15s
+    rrun ${cdate} fcst #&>/dev/null
+    rboot ${cdate} fcst #&>/dev/null
+  fi
+done
 
 if ${send_email}; then
   echo "${msg}" | mail -s "${subject}" ${recipents}
